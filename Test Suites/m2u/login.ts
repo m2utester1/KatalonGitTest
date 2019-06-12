876d37477d3b63ce8b5798082f054a85b8821705
Mohammad Mosaj Gohar <mohdmosaj.g@maybank.com>
2019-06-12 12:02:43 +0800

    hello jenkins

---
+++ Test Suites/m2u/login.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0f25e840-567f-4064-b011-4c816daea4ad</testSuiteGuid>
+   <testCaseLink>
+      <guid>7f584dd5-89db-4552-9df3-c58beb65e53d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/m2u login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
